32bce50596e5077539790a3485ac87ff76bc8eb2
Marc Allaume <marc.allaume@gmail.com>
2018-04-22 20:41:48 +0200

    Add project files.

---
+++ Pressford/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pressford.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
